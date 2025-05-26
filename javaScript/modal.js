e3d4658acf334b41b92b9f09e14fa7d72320130f
Shevchnko Evgeniy <zhenshev05@mail.ru>
2025-05-26 11:21:25 +0300

    fix-for-rule-BEM

---
+++ javaScript/modal.js
@@ -0,0 +1,16 @@
+function openModal(imageUrl) {
+    event.preventDefault(); 
+    document.getElementById('modalImage').src = imageUrl;
+    document.getElementById('modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target === modal) {
+        closeModal();
+    }
+}
